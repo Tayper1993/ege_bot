329ab2b2a0f71224723378b1e8dd6f19813a6fff
polyakov_ks@smart-transport.ru <polyakov_ks>
2024-10-29 17:00:04 +0500

    add column score + migration

--- core/models/users.py
+++ core/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer
+from sqlalchemy import String, SmallInteger, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.base import Base
@@ -11,6 +11,11 @@ class User(Base):
 
     __tablename__ = 'user'
 
+    __table_args__ = (
+        CheckConstraint('score >= 0 AND score <= 100', name='check_score_range'),
+    )
+
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False, comment='Идентификатор')
     name: Mapped[str] = mapped_column(String(1000), comment='Имя')
     surname: Mapped[str] = mapped_column(String(200), comment='Фамилия')
+    score: Mapped[int] = mapped_column(SmallInteger, comment='Баллы', default=0)
