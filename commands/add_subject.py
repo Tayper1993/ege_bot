25aff568d590bdcce156641abc31a4fd36438447
polyakov_ks@smart-transport.ru <polyakov_ks>
2024-10-30 10:51:44 +0500

    command subject

---
+++ commands/add_subject.py
@@ -0,0 +1,14 @@
+from core.models.subject import Subject
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class AddScoreCommand:
+    def __init__(self, name: str, session: AsyncSession):
+        self.name = name
+        self.session = session
+
+    async def execute(self):
+        add_subject = Subject(name=self.name)
+
+        async with self.session.begin():
+            self.session.add(add_subject)
