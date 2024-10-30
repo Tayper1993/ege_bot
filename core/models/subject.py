c515cf704ad965c255f77e3857f3efc1759161f1
polyakov_ks@smart-transport.ru <polyakov_ks>
2024-10-30 17:36:54 +0500

    init migration

--- core/models/subject.py
+++ core/models/subject.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Enum
+from sqlalchemy import Enum, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.base import Base
@@ -13,6 +13,6 @@ class Subject(Base):
     __tablename__ = 'subjects'
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False, comment='Идентификатор')
-    name: Mapped[SubjectEnum] = mapped_column(Enum(SubjectEnum), comment='Название школьного предмета')
+    name: Mapped[str] = mapped_column(String, comment='Название школьного предмета')
 
     scores = relationship('Score', back_populates='subject')
